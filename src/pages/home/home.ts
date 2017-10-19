3cceb9e46b955f9bbed29a9cd091efc6ebb1dabc
rafiromadhon <rafiromadhon006@gmail.com>
2017-10-19 16:18:07 +0700

    versi 1.0.0

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  splash = true;
+  //tabBarElement: any;
 
+  constructor(public navCtrl: NavController) {
+    //this.tabBarElement = document.querySelector('.tabbar');
   }
 
+  ionViewDidLoad() {
+    //this.tabBarElement.style.display = 'none';
+    setTimeout(() => {
+      this.splash = false;
+      //this.tabBarElement.style.display = 'flex';
+    }, 4000);
+  }
+
+  slides = [
+    {
+      title: "Welcome to the Docs!",
+      description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+      image: "assets/img/ica-slidebox-img-1.png",
+    },
+    {
+      title: "What is Ionic?",
+      description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+      image: "assets/img/ica-slidebox-img-2.png",
+    },
+    {
+      title: "What is Ionic Cloud?",
+      description: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+      image: "assets/img/ica-slidebox-img-3.png",
+    }
+  ];
 }
